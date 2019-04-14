19071af06548dbc0be96d2bfaa4211fe27e4e48a
Mike Mullen <mmullen227@gmail.com>
2019-04-14 18:04:39 -0400

    validate data part 2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -16,7 +16,7 @@ const User = mongoose.model('User', {
         type: Number,
         default: 0,
         validate(value) {
-            if(value < 0) {
+            if (value < 0) {
                 throw new Error('Age must be a positive number')
             }
         }
@@ -27,41 +27,59 @@ const User = mongoose.model('User', {
         trim: true,
         lowercase: true,
         validate(value) {
-            if(!validator.isEmail(value)) {
+            if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            // if(value.length <= 6) {
+            //     throw new Error('Password too short. Must be greater than 6 characters.')
+            // }
+            if(value.toLowerCase.includes('password')) {
+                throw new Error('"password" is not valid. Try again.')
             }
         }
     }
 })
 
-const me = new User({
-    name: '    Mike    ',
-    email: 'MYEMAIL@gmAIL.COM'
-})
+// const me = new User({
+//     name: '    Mike    ',
+//     email: 'MYEMAIL@gmAIL.COM',
+//     password: 'mongoose'
+// })
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false,
     }
 })
 
-// const task = new Task({
-//     description: 'wash car',
-//     completed: false
-// })
+const task = new Task({
+     description: '   Eat lunch',
+    // completed: false
+})
 
-// task.save().then(() => {
-//     console.log(task)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log('Error!', error)
+})

2840d48c0bce4fd7cdc7cbe1f02a5f6dca593cbd
Mike Mullen <mmullen227@gmail.com>
2019-04-14 22:47:51 -0400

    create task endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -19,17 +19,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 // })
 
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false,
-    }
-})
 
 // const task = new Task({
 //      description: '   Eat lunch',
